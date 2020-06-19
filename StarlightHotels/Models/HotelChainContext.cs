169096edf48b86529ab8bd247d842c43d5c4558f
philkoumi17 <philkoumi17@gmail.com>
2020-06-19 17:48:38 +0200

    Installation with some nugets and ApplicationUser models with generated tables on the DB

--- StarlightHotels/Models/HotelChainContext.cs
+++ StarlightHotels/Models/HotelChainContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace StarlightHotels.API.Models
 {
-    public class HotelChainContext
+    public class HotelChainContext : IdentityDbContext
     {
+        public HotelChainContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
--- StarlightHotels/Startup.cs
+++ StarlightHotels/Startup.cs
@@ -5,13 +5,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using StarlightHotels.API.Models;
 
-namespace StarlightHotels
+namespace StarlightHotels.API
 {
     public class Startup
     {
@@ -26,15 +29,23 @@ namespace StarlightHotels
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMvc();
+            services.AddDbContext<HotelChainContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("HotelChainConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<HotelChainContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if(env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
 
