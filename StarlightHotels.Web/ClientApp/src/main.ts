91fbf99f8bbdf123ced4563abcd17db3d5e0a0c1
philkoumi17 <philkoumi17@gmail.com>
2020-06-23 20:14:10 +0200

    Adding Angular template theme

--- StarlightHotels.Web/ClientApp/src/main.ts
+++ StarlightHotels.Web/ClientApp/src/main.ts
@@ -1,22 +1,25 @@
+/*!
+
+ =========================================================
+ * Paper Kit 2 Angular - v1.3.0
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/paper-kit-2-angular
+ * Copyright 2017 Creative Tim (https://www.creative-tim.com)
+ * Licensed under MIT (https://github.com/timcreative/paper-kit/blob/master/LICENSE.md)
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-export function getBaseUrl() {
-  return document.getElementsByTagName('base')[0].href;
-}
-
-const providers = [
-  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
-];
-
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic(providers).bootstrapModule(AppModule)
-  .catch(err => console.log(err));
-
-export { renderModule, renderModuleFactory } from '@angular/platform-server';
+platformBrowserDynamic().bootstrapModule(AppModule);
