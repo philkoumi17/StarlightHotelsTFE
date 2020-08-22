518ef604806afa7a10dedb29bb713481c20c2d48
philkoumi17 <philkoumi17@gmail.com>
2020-08-22 05:14:56 +0200

    Controller for room + views for the administration

---
+++ StarlightHotels.Data/ViewModels/HotelChambreViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarlightHotels.Models.ViewModels
+{
+    public class HotelChambreViewModel
+    {
+        public int NumChambre { get; set; }
+        public string Categorie { get; set; }
+    }
+}
